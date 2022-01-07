7d5731a696a6ed1ef17da1573594cae5b9d026f4
TheoC26 <91738250+TheoC26@users.noreply.github.com>
2022-01-06 19:52:23 -0500

    adding some responsiveness

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menuScreen = document.querySelector('.menu-screen');
+const menuBtn = document.querySelector(".menu-btn");
+const body = document.querySelector("body");
+const menuScreen = document.querySelector(".menu-screen");
+const downArrow = document.querySelector(".downarrow");
 let menuOpen = false;
-menuBtn.addEventListener('click', ()=> {
-    menuBtn.classList.toggle("open");
-    menuScreen.classList.toggle("open");
-    menuOpen = !menuOpen;
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+  menuScreen.classList.toggle("open");
+  body.classList.toggle("open");
+  menuOpen = !menuOpen;
+});
+downArrow.addEventListener("click", () => {
+  downArrow.scrollIntoView();
 });
